3734488e774de9fd27400f5545f4b1a1326d2c44
zaidbaig2 <zaidbaig2020@gmail.com>
2023-02-20 00:32:12 +0500

    main

---
+++ app.js
@@ -0,0 +1,48 @@
+var studentsName =[]
+console.log(studentsName)
+
+var arrays = ["", "" ,"" ,""]
+console.log(arrays)
+
+var numbers =[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+console.log(numbers)
+
+
+var booleans =[true,false,true,false,false,true,true]
+console.log(booleans)
+
+var mixedArrays =(1, "zaid", true)
+console.log(mixedArrays)
+
+var mobileNetworks =["ufone","jazz","telenor"]
+console.log(mobileNetworks)
+
+// var Qualification =["Qualification:"]
+
+var educationQualifications =["ssc", "hsc", "bcs", "bs", "bcom", "phd"]
+document.write(educationQualifications[0]) 
+document.write(educationQualifications[1])
+document.write(educationQualifications[2])
+document.write(educationQualifications[3])
+document.write(educationQualifications[4])
+document.write(educationQualifications[5])
+
+
+var movies =["mission majnu","thank god","phone bhoot","beast"]
+document.write(movies)
+
+var cars =["audi", "lamborgini", "ferari"]
+document.write(cars[1])
+document.write(cars[2])
+
+var colors=prompt["beginningcolor"]
+var color=prompt["endcolor"]
+
+var colored =[colors, color, "grey","golden"]
+document.write[colored]
+
+colored.shift()
+document.write[colored]
+
+colored.pop()
+document.write[colored]
